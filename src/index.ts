ba9a4f6ada8a42cd1af1c11e0c954fef0738d686
Bc05 <odilon.garcez.moraes@gmail.com>
2020-09-23 19:48:16 -0300

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// export const sum = (a: number, b: number) => {
+//   if ('development' === process.env.NODE_ENV) {
+//     console.log('ihuul');
+//   }
+console.log("xcfghj")
+  // return a + b;
+// };
